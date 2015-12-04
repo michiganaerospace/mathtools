3486a22aa86f3c7794c153d21fb0e6918833261c
Matthew J. Lewis <lightscalar@gmail.com>
2015-12-03 19:36:48 -0500

    Added some structure.

---
+++ mathtools/fit.py
@@ -0,0 +1,11 @@
+'''An interpolation object for fitting data with Splines, Legendre Polynomials, 
+and Fourier Series.'''
+import numpy as np    
+
+
+class Fit(object):
+
+    def __init__(self):
+        pass
+    
+
