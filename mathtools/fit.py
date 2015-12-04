ee81073625f641b60b4fd71546af4eee660d6213
Matthew J. Lewis <lightscalar@gmail.com>
2015-12-03 20:23:52 -0500

    Creating the Fit class.

--- mathtools/fit.py
+++ mathtools/fit.py
@@ -3,9 +3,10 @@ and Fourier Series.'''
 import numpy as np    
 
 
+# Fit class. A general purpose interpolation machine.
 class Fit(object):
 
-    def __init__(self):
+    def __init__(self, x=None, y=None, fit_method='legendre', reg_y=0,\
+                 reg_dy=None, reg_d2y=0):
         pass
     
-
