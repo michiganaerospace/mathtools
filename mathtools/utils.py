c500b4d9afa13f13404c89d0b4e98b5df5ba8f62
Matthew J. Lewis <lightscalar@gmail.com>
2015-12-06 23:19:32 -0500

    Type validation is now functional.

--- mathtools/utils.py
+++ mathtools/utils.py
@@ -1,6 +1,31 @@
-'''Utility functions for mathtools tools...'''
+'''Utility functions for mathtools... tools.'''
 import numpy as np
 
+
+def validate_type(var, white_list, var_name='Input'):
+    '''Ensure that the variable type is the supplied whitelist. 
+    INPUTS
+        var - variable or object
+            A valid Python variable or object.
+        white_list - list
+            A list of allowable types. If type(<var>) is not in <white_list>,
+            an error is thrown.
+    OUTPUTS
+        valid_type - boolean
+            True if the variable is in the whitelist. Otherwise an error is 
+            thrown with an appropriate error message.
+    '''
+    if type(var) in white_list:
+        return True
+    else:
+        error_message = '%s must be of type: ' % var_name
+        for idx, allowable_type in enumerate(white_list):
+            if idx == len(white_list)-1:
+                error_message += str(allowable_type)
+            else:
+                error_message += str(allowable_type) + ', or '
+        raise ValueError(error_message)
+   
 
 def scale_to_interval(x, interval):
     '''Shift and scale vector so that its elements live in the specified 
@@ -17,9 +42,9 @@ def scale_to_interval(x, interval):
             elements now live the within the interval specified in <interval>.
     '''
 
-    # Ensure we are working with numpy arrays.
-    if type(x) == list:
-        x = np.array(x)
+    # Ensure we are working with numpy arrays or lists.
+    validate_type(x, [list, np.ndarray])
+    validate_type(interval, [list, np.ndarray])
 
     if type(interval) == list:
         interval = np.array(interval)
@@ -30,7 +55,6 @@ def scale_to_interval(x, interval):
     # Apply appropriate shift.
     shift = x.mean() - interval.mean()
     x_ -= shift
-
 
     return x_
     
